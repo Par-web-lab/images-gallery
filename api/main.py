bc475f5a7cada203908900c24163680262a630df
Parneet <kaur.it.par@gmail.com>
2025-07-05 20:28:43 +0530

    enable debug mode in flask app

--- api/main.py
+++ api/main.py
@@ -7,6 +7,7 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL="https://api.unsplash.com/photos/random"
 UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY","")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
@@ -14,6 +15,7 @@ if not UNSPLASH_KEY:
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = DEBUG
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
